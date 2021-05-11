23b3e66744ed7a16bbb698f0da143c1e0cc50593
dcyljr <310547343@qq.com>
2021-05-11 22:44:07 +0800

    Add:搭建登录接口并验证

--- System/front/routes/api/users.js
+++ System/front/routes/api/users.js
@@ -2,7 +2,10 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
-
+const jwt = require('jsonwebtoken');
+const gravatar = require('gravatar');
+const keys = require('../../config/keys');
+const passport = require('passport');
 const User = require('../../models/User');
 
 
@@ -18,6 +21,7 @@ router.post("/register", (req, res) => {
             if (user) {
                 return res.status(400).json({ email: "邮箱已被注册！" })
             } else {
+                const avatar = gravatar.url(req.body.email, { s: '200', r: 'pg', d: 'mm' });
                 const newUser = new User({
                     name: req.body.name,
                     email: req.body.email,
@@ -38,6 +42,43 @@ router.post("/register", (req, res) => {
                 })
             }
         })
+})
+
+router.post("/login", (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    User.findOne({ email })
+        .then(user => {
+            if (!user) {
+                return res.status(404).json({ email: "用户不存在！" });
+            }
+
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if (isMatch) {
+                        const rule = { id: user.id, name: user.name };
+                        jwt.sign(rule, keys.secretOrKey, { expiresIn: 3600 }, (err, token) => {
+                            if (err) throw err;
+                            res.json({
+                                success: true,
+                                token: "Bearer " + token
+                            })
+                        })
+                        // res.json({ msg: "success" });
+                    } else {
+                        return res.status(400).json({ password: "密码错误！" })
+                    }
+
+                });
+        })
+})
+
+router.get("/current", passport.authenticate("jwt", { session: false }), (req, res) => {
+    res.json({
+        id: req.user.id,
+        name: req.user.name,
+        email: req.user.email
+    });
 })
 
 module.exports = router;
--- System/front/server.js
+++ System/front/server.js
@@ -1,22 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const passport = require('passport');
 const app = express();
 
 const users = require('./routes/api/users');
 
 const db = require('./config/keys').mongoURI;
 
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 mongoose.connect(db)
     .then(() => console.log("MongoDB Connected"))
     .catch(err => console.log(err));
 
-app.get('/', (req, res) => {
-    res.send('业绩管理系统数据库');
-})
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
+// app.get('/', (req, res) => {
+//     res.send('业绩管理系统数据库');
+// })
 
 app.use('/api/users', users);
 
