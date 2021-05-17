54a3e843cd8ab7716e22612f493636413d93c742
dcyljr <310547343@qq.com>
2021-05-17 17:10:00 +0800

    Add:添加profiles.js

---
+++ System/front/routes/api/profiles.js
@@ -0,0 +1,13 @@
+//login&register
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const Profile = require('../../models/Profile');
+
+
+router.get('/test', (req, res) => {
+    res.json({ msg: "profile works" })
+})
+
+
+module.exports = router;
